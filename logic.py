efd1cbff4a2ad3322e0aedd7bc3cde0552a10045
joehuang1108 <joehuang1108@gmail.com>
2023-10-29 18:26:02 -0700

    Initial commit of 2048 game

---
+++ logic.py
@@ -0,0 +1,27 @@
+
+# This is the file that contains all the logic in the game
+# 4 x 4 grid with directions to move up, down, left, or right
+
+import random
+
+# Make a function that initializes a game/grid at the start
+def start_game():
+    # [0] [0] [0] [0]
+    # [0] [0] [0] [0]
+    # [0] [0] [0] [0]
+    # [0] [0] [0] [0]
+    mat = []
+    for i in range(4):
+        mat.append([0] * 4)
+
+    add_new_2(mat)
+    return mat
+
+# choose a random row and column to add a 2
+def add_new_2(mat):
+    r = random.randint(0, 3)
+    c = random.randint(0, 3)
+
+    # check empty before assigning
+
+    mat[r][c] = 2
---
+++ main.py
@@ -0,0 +1,10 @@
+# 2048
+
+import logic
+
+
+mat = logic.start_game()
+for x in mat:
+    print(x)
+
+print("This is main")
